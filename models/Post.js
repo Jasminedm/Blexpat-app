8a868e38882c0467558b0befc437817e6bc0b464
Jasminedm <mooredjasmine01@gmail.com>
2022-11-28 17:17:09 -0500

    first form working with database

--- models/Post.js
+++ models/Post.js
@@ -3,67 +3,67 @@ const mongoose = require("mongoose");
 const PostSchema = new mongoose.Schema({
   location: {
     type: String,
-    required: true,
-  },
-  postImg: {
-    type: String,
-    require: true,
+    
   },
   cloudinaryId: {
     type: String,
     require: true,
   },
-  duration: {
+  arrive: {
         type: Date,
         default: Date.now,
   },
+  depart: {
+    type: Date,
+    default: Date.now,
+},
   budgetPaid: {
     type: String,
-    required: true,
+    
   },
   exp: {
     type: String,
-    required: true,
+    
   },
   room: {
     type: String,
-    required: true,
+    
   },
   healthcare: {
     type: String,
-    required: true,
+    
   },
   allergy: {
     type: String,
-    required: true,
+    
   },
   attractions: {
     type: String,
-    required: true,
+    
   },
   transp: {
     type: String,
-    required: true,
+    
   },
   handicap: {
     type: String,
-    required: true,
+    
   },
   shop: {
     type: String,
-    required: true,
+    
   },
   trouble:{
     type: String,
-    required: true,
+    
   },
   kidfriendly: {
     type: String,
-    required: true,
+    
   },
   petfriendly: {
     type: String,
-    required: true,
+    
   },
   user: {
     type: mongoose.Schema.Types.ObjectId,
